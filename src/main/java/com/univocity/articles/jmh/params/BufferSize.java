9264f0544c8d087350e878d2e0ef2f6c7c127a21
Arnaud Roger <arnaud.roger@gmail.com>
2014-10-20 20:23:52 +0100

    update sfm version

--- src/main/java/com/univocity/articles/jmh/params/BufferSize.java
+++ src/main/java/com/univocity/articles/jmh/params/BufferSize.java
@@ -1,10 +1,10 @@
 package com.univocity.articles.jmh.params;
 
 public enum BufferSize {
-	_1K(10), _4K(12), _8K(13), _16K(14), _32K(15), _64K(16), _128K(17), _1M(20), _4M(22);
+	D2K(2000), D4K(4000), _1K(1<< 10), _2K(1<< 11), _4K(1<<12), _8K(1<<13), _16K(1<<14), _32K(1<<15), _64K(1<<16), _128K(1<<17), _256K(1<<18), _512K(1<<19), _1M(1<<20), _4M(1<<22);
 	
 	BufferSize(int n) {
-		this.size = 1 << n;
+		this.size = n;
 	}
 	
 	public final int size;
