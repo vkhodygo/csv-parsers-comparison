75f67335f55ee1b822a331c6f4842e5f2f7cc15d
Arnaud Roger (e19224) <Arnaud.Roger@cmegroup.com>
2014-10-23 20:49:43 +0100

    add bean io jmh

---
+++ src/main/java/com/univocity/articles/jmh/BeanIoParser.java
@@ -0,0 +1,37 @@
+package com.univocity.articles.jmh;
+
+import java.util.concurrent.TimeUnit;
+
+import org.beanio.stream.csv.CsvReader;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+
+import com.univocity.articles.jmh.params.FileToProcess;
+
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Fork(1)
+@Warmup(iterations=5,batchSize=1)
+@Measurement(iterations=5,batchSize=1)
+public class BeanIoParser {
+	
+	@Benchmark
+	public void parseFile(final FileToProcess fileToProcess, final Blackhole blackhole) throws Exception {
+		CsvReader reader = new CsvReader(fileToProcess.getReader());
+		try {
+			String[] data;
+			while((data = reader.read())!= null)  {
+				blackhole.consume(data);
+			}
+		} finally {
+			reader.close();
+		}
+	}
+}
